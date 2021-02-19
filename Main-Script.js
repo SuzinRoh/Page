2f970298e9e605ee8ba5d183457e6477a2302109
SujinRoh <suuz.hha@gmail.com>
2021-02-20 02:21:17 +0900

    210220 add js

---
+++ Main-Script.js
@@ -0,0 +1,23 @@
+//<i class="far fa-dizzy"></i> 
+
+function handleClick() { 
+   let btn = document.querySelector('.nav-button');
+   let menu = document.querySelector('.nav-items');
+   var NAME = 'clicked';
+  
+   if(!btn.classList.contains(NAME)){
+        menu.style.right = '0';
+        btn.style.right = '0';
+        btn.style.color = 'rgb(66, 132, 255)';
+        btn.classList.add('clicked');
+        btn.innerHTML = '<span><i class="far fa-dizzy fa-lg"></i></span>';
+   } else{
+        menu.style.right = '-90px';
+        btn.style.color = 'black';
+        btn.style.right = '-90px';
+        btn.classList.remove('clicked');
+        btn.innerHTML = '<span ><i class="far fa-smile fa-lg"></i></span>';
+   }
+}
+
+document.addEventListener('click' , handleClick);
