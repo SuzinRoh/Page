d86f725424a4d45db6d25e1bb2715b98bc8389ee
SujinRoh <suuz.hha@gmail.com>
2021-02-21 02:25:36 +0900

    210221

--- Main-Script.js
+++ Main-Script.js
@@ -1,23 +1,49 @@
-//<i class="far fa-dizzy"></i> 
 
+//변수
+const navbtn = document.querySelector('.nav-button');
+
+// nva btn cilck event
 function handleClick() { 
-   let btn = document.querySelector('.nav-button');
    let menu = document.querySelector('.nav-items');
-   var NAME = 'clicked';
-  
-   if(!btn.classList.contains(NAME)){
-        menu.style.right = '0';
-        btn.style.right = '0';
-        btn.style.color = 'rgb(94, 66, 255)';
-        btn.classList.add('clicked');
-        btn.innerHTML = '<span><i class="far fa-dizzy fa-lg"></i></span>';
-   } else{
-        menu.style.right = '-90px';
-        btn.style.color = 'black';
-        btn.style.right = '-90px';
-        btn.classList.remove('clicked');
-        btn.innerHTML = '<span ><i class="far fa-smile fa-lg"></i></span>';
+   let icon = document.querySelector('.image');
+   let NAME = 'clicked';
+
+   if(!navbtn.classList.contains(NAME)){
+        menu.style.right = '10px';
+        navbtn.classList.add('clicked');
+        icon.classList.remove('smile-img');
+        icon.classList.add('dizzy-img');
+   } else {
+        menu.style.right = '-110px';
+        navbtn.classList.remove('clicked');
+        icon.classList.remove('dizzy-img');
+        icon.classList.add('smile-img');
    }
 }
+navbtn.addEventListener('click' , handleClick);
 
-document.addEventListener('click' , handleClick);
+
+//btn scrolling event
+let isScrolling;
+// Listen for scroll events
+window.addEventListener('scroll', function ( event ) {
+    navbtn.classList.remove('showBtn');
+    navbtn.classList.add('hideBtn');
+	// Clear our timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+        navbtn.classList.remove('hideBtn');
+        navbtn.classList.add('showBtn');
+		// Run the callback
+		//console.log( 'Scrolling has stopped.' );
+	});
+
+}, false);
+/*var scrollStop = function (callback) {
+   
+}
+scrollStop(function () {
+    
+    console.log( 'Scrolling has stopped.' );
+});*/
